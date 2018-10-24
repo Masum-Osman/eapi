c135b4d61e35ed5a1def69b49f09ca06e99fb629
Masum-Osman <ogkmasum@gmail.com>
2018-10-24 12:08:22 +0600

    laravel passport added. my first usases of Postman to create API

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -23,8 +25,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
         $this->registerPolicies();
 
-        //
+        Passport::routes();
     }
 }
